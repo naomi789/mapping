6212084fb7017d6cab6a4c0c41925640fb23e607
Naomi Johnson <snjohnson789@gmail.com>
2023-04-30 17:30:44 -0700

    importing Seattle map

---
+++ mapping.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+import matplotlib.pyplot as plt
+
+# get map of seattle from: https://data-seattlecitygis.opendata.arcgis.com/
+street_map = gpd.read_file('/data/Seattle_City_Limits.shp')
+
+# https://towardsdatascience.com/geopandas-101-plot-any-data-with-a-latitude-and-longitude-on-a-map-98e01944b972
+# https://medium.com/@ianforrest11/graphing-latitudes-and-longitudes-on-a-map-bf64d5fca391
