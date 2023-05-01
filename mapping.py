967dda095bc1227b5c4f804acb12bb9252f7c9e0
Naomi Johnson <snjohnson789@gmail.com>
2023-04-30 18:21:42 -0700

    fixing location

--- mapping.py
+++ mapping.py
@@ -5,7 +5,7 @@ from shapely.geometry import Point, Polygon
 import matplotlib.pyplot as plt
 
 # get map of seattle from: https://data-seattlecitygis.opendata.arcgis.com/
-street_map = gpd.read_file('/data/Seattle_City_Limits.shp')
+street_map = gpd.read_file('./data/Seattle_City_Limits.shp')
 
 # https://towardsdatascience.com/geopandas-101-plot-any-data-with-a-latitude-and-longitude-on-a-map-98e01944b972
 # https://medium.com/@ianforrest11/graphing-latitudes-and-longitudes-on-a-map-bf64d5fca391
